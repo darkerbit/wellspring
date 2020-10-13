48d1d337c883454298f0203af05b6b588efc2711
DarkerBit <mail.darkerbit@gmail.com>
2020-10-13 07:57:59 +0300

    Boilerplate and relicense
    Signed-off-by: DarkerBit <mail.darkerbit@gmail.com>

---
+++ src/main/java/io/github/darkerbit/wellspring/client/WellspringClientMod.java
@@ -0,0 +1,10 @@
+package io.github.darkerbit.wellspring.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WellspringClientMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hello, world!");
+    }
+}
